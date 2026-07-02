17bf51db1f761aa325ebf678b1ebcbfb856c5606
OEddie <edsonnfi00@gmail.com>
2026-07-02 20:13:42 -0300

    Initial commit

---
+++ include/mira/colorcodes.h
@@ -0,0 +1,87 @@
+#ifndef COLORCODES_H_
+#define COLORCODES_H_ 
+
+//RESET
+
+#define CLEARCOLOR "\e[0m"
+
+//REGULAR COLORS - CC
+
+#define CC_BLACK  "\e[0;30m"
+#define CC_RED    "\e[0;31m"
+#define CC_GREEN  "\e[0;32m"
+#define CC_YELLOW "\e[0;33m"
+#define CC_BLUE   "\e[0;34m"
+#define CC_PURPLE "\e[0;35m"
+#define CC_CYAN   "\e[0;36m"
+#define CC_WHITE  "\e[0;37m"
+
+//BOLD - CCB
+
+#define CCB_BLACK  "\e[1;30m"
+#define CCB_RED    "\e[1;31m"
+#define CCB_GREEN  "\e[1;32m"
+#define CCB_YELLOW "\e[1;33m"
+#define CCB_BLUE   "\e[1;34m"
+#define CCB_PURPLE "\e[1;35m"
+#define CCB_CYAN   "\e[1;36m"
+#define CCB_WHITE  "\e[1;37m"
+
+//UNDERLINE - CCUL
+
+#define CCUL_BLACK  "\e[4;30m"
+#define CCUL_RED    "\e[4;31m"
+#define CCUL_GREEN  "\e[4;32m"
+#define CCUL_YELLOW "\e[4;33m"
+#define CCUL_BLUE   "\e[4;34m"
+#define CCUL_PURPLE "\e[4;35m"
+#define CCUL_CYAN   "\e[4;36m"
+#define CCUL_WHITE  "\e[4;37m"
+
+//BACKGROUND CCBG
+
+#define CCBG_BLACK  "\e[40m"
+#define CCBG_RED    "\e[41m"
+#define CCBG_GREEN  "\e[42m"
+#define CCBG_YELLOW "\e[43m"
+#define CCBG_BLUE   "\e[44m"
+#define CCBG_PURPLE "\e[45m"
+#define CCBG_CYAN   "\e[46m"
+#define CCBG_WHITE  "\e[47m"
+
+//HIGH INTENSITY - CCHI
+
+#define CCHI_BLACK  "\e[0;90m"
+#define CCHI_RED    "\e[0;91m"
+#define CCHI_GREEN  "\e[0;92m"
+#define CCHI_YELLOW "\e[0;93m"
+#define CCHI_BLUE   "\e[0;94m"
+#define CCHI_PURPLE "\e[0;95m"
+#define CCHI_CYAN   "\e[0;96m"
+#define CCHI_WHITE  "\e[0;97m"
+
+//BOLD HIGH INTENSITY - CCBHI
+
+#define CCBHI_BLACK  "\e[1;90m"
+#define CCBHI_RED    "\e[1;91m"
+#define CCBHI_GREEN  "\e[1;92m"
+#define CCBHI_YELLOW "\e[1;93m"
+#define CCBHI_BLUE   "\e[1;94m"
+#define CCBHI_PURPLE "\e[1;95m"
+#define CCBHI_CYAN   "\e[1;96m"
+#define CCBHI_WHITE  "\e[1;97m"
+
+//HIGH INTENSITY BACKGROUNDS - CCHIBG
+
+#define CCHIBG_BLACK  "\e[0;100m"
+#define CCHIBG_RED    "\e[0;101m"
+#define CCHIBG_GREEN  "\e[0;102m"
+#define CCHIBG_YELLOW "\e[0;103m"
+#define CCHIBG_BLUE   "\e[0;104m"
+#define CCHIBG_PURPLE "\e[0;105m"
+#define CCHIBG_CYAN   "\e[0;106m"
+#define CCHIBG_WHITE  "\e[0;107m"
+
+//
+
+#endif
